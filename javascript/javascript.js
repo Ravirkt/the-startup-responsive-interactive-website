320433e068b30e6ea4089f2b7d18c9b7400d3f12
Ravirkt <r_tjikhoeri@live.com>
2025-01-19 17:17:26 +0100

    scroll event for visibility navbar #1

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -9,3 +9,36 @@ function scrollUp () {
     behavior: "smooth" 
   });
 }
+
+
+
+
+
+
+
+
+// navbar
+
+const nav = document.querySelector(".navbar");
+let lastScrollY = window.scrollY;
+
+
+window.addEventListener("scroll", scrollUpDown);
+
+function scrollUpDown() {
+  
+  if (lastScrollY < window.scrollY) {
+    nav.classList.add("nav--hidden");
+  } else {
+    nav.classList.remove("nav--hidden");
+  }
+
+
+  if (window.scrollY > 1) {
+    nav.classList.add("nav--scrolled");
+  } else {
+    nav.classList.remove("nav--scrolled");
+  }
+
+  lastScrollY = window.scrollY;
+}
